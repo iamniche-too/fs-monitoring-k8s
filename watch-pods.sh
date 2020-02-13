7b9d9336880dcecf79b2c81a00b98402c0a162c7
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-02-13 16:30:42 +0000

    Tweaks to Prometheus,

--- watch-pods.sh
+++ watch-pods.sh
@@ -1,4 +1,1 @@
-export GOOGLE_APPLICATION_CREDENTIALS=./terraform-test-262517-b4ff2f611b21.json
-./generate-cluster-connection-yaml.sh
-
-watch kubectl -n monitoring get pods --kubeconfig ./kubeconfig.yaml
+watch ./get-pods.sh 
